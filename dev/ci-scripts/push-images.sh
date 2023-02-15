543b3c5f207cfa1f81f9dcd3d0acaa140fa79b5e
sathoune <michal5bargiel@gmail.com>
2023-02-15 14:09:18 +0100

    🎉 Push images to Docker Registry.

--- dev/ci-scripts/push-images.sh
+++ dev/ci-scripts/push-images.sh
@@ -3,4 +3,10 @@ set -e
 docker compose up -d --build
 docker compose down --remove-orphans
 
+docker tag app-read-backend registry:5000/read-backend
+docker tag app-write-backend registry:5000/write-backend
+docker tag app-frontend registry:5000/frontend
 
+docker push registry:5000/read-backend
+docker push registry:5000/write-backend
+docker push registry:5000/frontend
